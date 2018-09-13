3f2b780319473f04db546de7ea83fc39082ef00a
zhangZG <zhangzhenguo@yxqiche.com>
2018-09-13 14:19:34 +0800

    add

---
+++ 小小node服务器-A-L-Y/S/api.js
@@ -0,0 +1,111 @@
+
+// 这个文件演示了 ，node.js 去调用了 微信的接口
+
+
+var express = require('express');
+var http = require('http');
+var path = require('path')
+var fs = require('fs')
+var bodyParser = require('body-parser');
+var request = require('request');
+var md5 = require('md5');
+
+var token = '';
+var access_token  = ''; // 第一步 
+var jsapi_ticket = ''; // 第二步
+var app_id = 'wxdb0b83006fe07fa6';
+var app_secret = 'd28ba63b80b40f6a970ab6c15cc2e034'
+
+// 生成 随机 32位的字符串
+function strFn(A_length){
+    var data=["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+    var result="";
+    for(var i=0;i<A_length;i++){
+        var r=Math.floor(Math.random()*62);     //取得0-62间的随机数，目的是以此当下标取数组data里的值！
+        result+=data[r];        //输出20次随机数的同时，让rrr加20次，就是20位的随机字符串了。
+    }
+    return result;
+}
+// 用三组字符串，进行加密
+var str = 'aaaabbsssbsaewHHHSDKkflwklkclwl3434123d'
+// 盐（自己写 一段自己都不认识的 字母）
+var yan = 'JJKkkkfkk9c949vjJJJjvwi4izk  kk';
+
+function md5Fn(length) {
+
+    return md5(md5(md5(`${strFn(length)}${str}${yan}`)))
+}
+
+var app = express();
+app.use(bodyParser.urlencoded({extended:true}))
+app.get('/', (req, res) => {
+    console.log('req.body',req.query.code)
+    console.log('md5',)
+      // res.send({'8999':111})
+
+    // 第一种 get 方式
+    // 这里去调用了  另外一个接口， 就可以认为是调用了 微信的接口  
+    request(`https://api.weixin.qq.com/sns/jscode2session?appid=${app_id}&secret=${app_secret}&js_code=${req.query.code}&grant_type=authorization_code`, function (error, response, body) {
+      if ( !response.errcode && response.access_token) {
+        if(!jsapi_ticket ) {
+            request(`https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token=${response.access_token}&type=jsapi`, function (error, resData, body) {
+                if(resData.errcode == 0) {
+                    jsapi_ticket = resData.ticket
+                    res.send({signature: resData.ticket, body: body})
+                } else {
+                    res.send({'resData 第二个接口 error': resData.errmsg})
+                }
+            })
+            // access_token 失效   jsapi_ticket的有效期为7200秒
+            setTimeout( function() {
+                jsapi_ticket = '';
+            }, 300000)
+        }
+        // if(!token) { // 没有token 去生成 token,  有token 用token
+        //     token = md5Fn(32);
+        //     // 5分钟后token 失效
+        //     setTimeout(function() {
+        //         token = '';
+        //     },300000)
+        // }
+        // res.send({token: token, body: body})
+      } else {
+        res.send({'error': response.errcode})
+      }
+    })
+
+    // 第二种 post 方式
+    // request({
+    //     url: 'http://localhost:8888/',
+    //     method: "POST",
+    //     json: true,
+    //     headers: {
+    //         "content-type": "application/json",
+    //     },
+    //     data: {
+    //         js_code: req.query,
+    //         appid: 'wxdb0b83006fe07fa6',
+    //         secret: 'd28ba63b80b40f6a970ab6c15cc2e034',
+    //         grant_type: 'authorization_code'
+    //     },
+    //     // body: JSON.stringify({name:1})
+    //     body: {"aaa": "111"}
+    // }, function(error, response, body) {
+    //     console.log('response ',response)
+    //     console.log('body ',body)
+    //     console.log('!error ',error)
+    //     console.log('response.statusCode == 200',response.statusCode == 200)
+    //     console.log('!error && response.statusCode == 200',!error && response.statusCode == 200)
+    //     if (!error && response.statusCode == 200) {
+    //         console.log('body',body)
+
+    //     }
+    //     res.send({'8999':111})
+    // }); 
+})
+
+
+app.listen(6000,() => {
+    console.log('服务启动成功 6000')
+})
---
+++ 小小node服务器-A-L-Y/S/js/share.js
@@ -0,0 +1,101 @@
+﻿(function () {
+    //title为微信分享的主标题
+    var _title = '火码互动-中国资深移动互联服务商';
+    //dese为副标题（详细描述）
+    var _desc = '你还在靠推送图文消息来吸引粉丝们的眼球？你OUT了！来看看火码在玩什么？';
+    //link为分享页所在的地址
+    var _link = 'https://www.zhangzhenguo1.com';
+    //_imgUrl 为 分享小图标的链接
+    var _imgUrl = 'https://gss2.bdstatic.com/-fo3dSag_xI4khGkpoWK1HF6hhy/baike/c0%3Dbaike80%2C5%2C5%2C80%2C26/sign=be3a8edb2cf5e0fefa1581533d095fcd/cefc1e178a82b90104b542f1728da9773812efdc.jpg';
+    var url = window.location.href.split("#")[0];
+    var signPackage;
+    var info = {
+        appId: '',
+        secret: '',
+        url: url
+    };
+    let n = new Date().getTime()
+
+    $.ajax({
+        type: "GET",
+        // dataType: "jsonp",
+        //url需要后台php返回一段数据，，具体是什么数据，我忘了，，文档上有写
+        url: `https://www.zhangzhenguo1.com/wxJssdk?timestamp=${n}&nonce=22222312312cxsd&appId=wxb1d34a0e7f8262dd`,
+        // data: {"param": JSON.stringify(info)},
+        async: false,
+        success: function (data) {
+            console.dir(data);
+            wx.config({
+                debug: true,
+                appId: data.appId,
+                timestamp: data.timestamp,
+                nonceStr: data.nonceStr,
+                signature: data.signature,
+                jsApiList: [
+                    // 所有要调用的 API 都要加到这个列表中
+                    'checkJsApi',
+                    'onMenuShareTimeline',
+                    'onMenuShareAppMessage',
+                    'onMenuShareQQ',
+                    'onMenuShareWeibo',
+                    'hideMenuItems',
+                    'showMenuItems',
+                    'hideAllNonBaseMenuItem',
+                    'showAllNonBaseMenuItem',
+                    'getNetworkType',
+                    'openLocation',
+                    'getLocation',
+                    'hideOptionMenu',
+                    'showOptionMenu',
+                    'closeWindow'
+                ]
+            });
+
+            wx.ready(function () {
+                // 在这里调用 API
+                // 2. 分享接口
+                // 2.1 监听“分享给朋友”，按钮点击、自定义分享内容及分享结果接口
+
+                wx.onMenuShareAppMessage({
+                    title: _title,
+                    desc: _desc,
+                    link: _link,
+                    imgUrl: _imgUrl,
+                    trigger: function (res) {
+                        //alert('用户点击发送给朋友');
+                    },
+                    success: function (res) {
+                        //alert('已分享');
+                    },
+                    cancel: function (res) {
+                        //alert('已取消');
+                    },
+                    fail: function (res) {
+                        //alert(JSON.stringify(res));
+                    }
+                });
+
+
+                // 2.2 监听“分享到朋友圈”按钮点击、自定义分享内容及分享结果接口
+
+                wx.onMenuShareTimeline({
+                    title: _title,
+                    link: _link,
+                    imgUrl: _imgUrl,
+                    trigger: function (res) {
+                        // alert('用户点击分享到朋友圈');
+                    },
+                    success: function (res) {
+                        // alert('已分享');
+                    },
+                    cancel: function (res) {
+                        // alert('已取消');
+                    },
+                    fail: function (res) {
+                        // alert(JSON.stringify(res));
+                    }
+                });
+            });	//end of wx.ready
+        }
+    })
+})();
